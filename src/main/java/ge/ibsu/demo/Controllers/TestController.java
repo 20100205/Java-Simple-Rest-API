35053c1e5ce10c392f0cad8c88584d24a3071d2f
20100205 <20100205@ibsu.edu.ge>
2023-03-25 14:22:00 +0400

    init-project

---
+++ src/main/java/ge/ibsu/demo/Controllers/TestController.java
@@ -0,0 +1,23 @@
+package ge.ibsu.demo.Controllers;
+
+import ge.ibsu.demo.DTO.Test;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @RequestMapping(value = "/call", method = RequestMethod.GET, produces = {"application/json"})
+    public Test test(){
+        return new Test("გიორგი", "მაისურაძე");
+    }
+
+    @RequestMapping(value = "/post", method = RequestMethod.POST, produces = {"application/json"})
+    public Test testPost(@RequestBody Test t){
+        System.out.println(t.getFirstName() + " " + t.getLastName());
+        return new Test(t.getFirstName() + " From Server ", t.getLastName());
+    }
+}
